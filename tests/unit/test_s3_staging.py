ddcf90716f5149a1a0f38d34aeb20880a01f3a61
Christian Kanesan <christian.kanesan@meteoswiss.ch>
2023-10-17 15:40:02 +0200

    Use side effect

--- tests/unit/test_s3_staging.py
+++ tests/unit/test_s3_staging.py
@@ -7,9 +7,11 @@ from minio import Minio
 
 @mock.patch('polytope_server.common.staging.s3_staging.Minio', autospec=True)
 def test_s3_staging_secure_false(mock_minio: mock.MagicMock):
-   s3_staging = S3Staging(config={})
-   
+   def callback(*args, **kwargs):
+      if kwargs.get("secure") is not True:
+         raise AssertionError("Secure arg value.")
+      return mock.MagicMock(autospec=Minio)
 
-   assert mock_minio.call_args_list is None
+   mock_minio.side_effect = callback
 
-        
+   S3Staging(config={})
