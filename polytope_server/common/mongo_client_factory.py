7161c9e82ed90daf7212627e35d45ce2c575bc18
Stefan Friedli <stefan.friedli@zuehlke.com>
2023-11-13 11:04:54 +0100

    Updates

--- polytope_server/common/mongo_client_factory.py
+++ polytope_server/common/mongo_client_factory.py
@@ -13,8 +13,8 @@ def create_client(
     endpoint = f"{host}:{port}"
 
     if username and password:
-        encoded_username = urllib.parse.quote_plus(f"{username}")
-        encoded_password = urllib.parse.quote_plus(f"{password}")
+        encoded_username = urllib.parse.quote_plus(username)
+        encoded_password = urllib.parse.quote_plus(password)
         endpoint = f"{encoded_username}:{encoded_password}@{endpoint}"
 
     return pymongo.MongoClient(endpoint, journal=True, connect=False, tls=tls)
