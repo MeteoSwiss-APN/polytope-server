f1cc434389521f286e734787cf695d3308558e4d
Miquel Puig <miquel.puigrais@zuehlke.com>
2024-11-13 17:04:20 +0100

    Added custom instrumentation in datasources and context propagation

--- polytope_server/common/observability/otel.py
+++ polytope_server/common/observability/otel.py
@@ -19,6 +19,20 @@ def add_trace_context(request):
 
     logging.debug(f"[OTEL] Request created with carrier: {request.otel_trace_ctx['carrier']}")
 
+def update_trace_context(request):
+    carrier = {}
+
+    # Inject the context into the carrier
+    inject(carrier)
+
+    # Adding trace_id and span_id to the request data for logging
+    request.otel_trace_ctx = {
+        'carrier': carrier  # Store the injected carrier
+    }
+
+    logging.debug(f"[OTEL] Request created with carrier: {request.otel_trace_ctx['carrier']}")
+
+
 def restore_trace_context(request):
     # If otel context is not set, return
     if not hasattr(request, 'otel_trace_ctx'):
@@ -34,25 +48,39 @@ def restore_trace_context(request):
 
     return extracted_context
 
+@contextmanager
+def create_new_span_internal(span_name, request_id=None, parent_context=None, kind=trace.SpanKind.INTERNAL):
+    with create_new_span(span_name, request_id, parent_context, kind) as span:
+        span.set_attribute("role", "internal")
+        yield span
 
+# Forcing span kind Server because of AWS representation
+@contextmanager
 def create_new_span_producer(span_name, request_id=None, parent_context=None, kind=trace.SpanKind.SERVER):
-    return create_new_span(span_name, request_id, parent_context, kind)
+    with create_new_span(span_name, request_id, parent_context, kind) as span:
+        span.set_attribute("role", "producer")
+        yield span
 
+# Forcing span kind Server because of AWS representation
+@contextmanager
 def create_new_span_consumer(span_name, request_id=None, parent_context=None, kind=trace.SpanKind.SERVER):
-    return create_new_span(span_name, request_id, parent_context, kind)
+    with create_new_span(span_name, request_id, parent_context, kind) as span:
+        span.set_attribute("role", "consumer")
+        yield span
+
+@contextmanager
+def create_new_span_server(span_name, request_id=None, parent_context=None, kind=trace.SpanKind.SERVER):
+    with create_new_span(span_name, request_id, parent_context, kind) as span:
+        span.set_attribute("role", "server")
+        yield span
 
 @contextmanager
 def create_new_span(span_name, request_id=None, parent_context=None, kind=trace.SpanKind.SERVER):
     tracer = trace.get_tracer(__name__)
 
     with tracer.start_as_current_span(span_name, context=parent_context, kind=kind, attributes={"polytope.request.id": request_id }) as span:
-        # if parent_context:
-        #     # Extract SpanContext from the parent_context
-        #     parent_span = trace.get_current_span(parent_context)
-        #     span_context = parent_span.get_span_context()
-        #     # Only add the link if the SpanContext is valid
-        #     if span_context.is_valid:
-        #         span.add_link(span_context)
-
-        logging.debug(f"[OTEL] Creating new span: {span_name}")
+        logging.debug(f"[OTEL] Creating new span: {span_name} parent: {parent_context}")
         yield span
+
+def set_span_error(span, exception):
+    span.set_status(trace.Status(trace.StatusCode.ERROR, str(exception)))
